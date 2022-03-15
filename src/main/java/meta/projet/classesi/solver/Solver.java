dbba87078d53d42c7f27ce3ecf98013a64e0230a
Ayoub Hammal <ayoubhammal@gmail.com>
2022-03-15 21:53:52 +0100

    solver abstract class

--- src/main/java/meta/projet/classesi/solver/Solver.java
+++ src/main/java/meta/projet/classesi/solver/Solver.java
@@ -1,6 +1,14 @@
 package meta.projet.classesi.solver;
 
+import java.util.Collection;
 
-public interface Solver {
-    public Node solve();
+public abstract class Solver {
+    private Node solution;
+    private Collection<Node> opened;
+    private Collection<Node> closed;
+
+    public abstract boolean solve();
+    public abstract Node getSolution();
+    public abstract Collection<Node> getOpened();
+    public abstract Collection<Node> getClosed();
 }
